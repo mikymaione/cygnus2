71f10f8aa1f39fabd0c6e3998fcc87ef163cfd54
Michele Maione <mikymaione@hotmail.it>
2024-03-15 16:30:36 +0100

    feat: birthday editor

--- lib/ui/mad/mad_crud.dart
+++ lib/ui/mad/mad_crud.dart
@@ -11,7 +11,9 @@ import 'package:cygnus2/data_structures/mad_data.dart';
 import 'package:cygnus2/data_structures/my_data.dart';
 import 'package:cygnus2/store/store_mad.dart';
 import 'package:cygnus2/ui/base/msg.dart';
+import 'package:cygnus2/ui/forms/date_editor.dart';
 import 'package:cygnus2/ui/forms/province_or_city_form.dart';
+import 'package:cygnus2/ui/forms/text_editor.dart';
 import 'package:cygnus2/ui/mad/city_dao.dart';
 import 'package:cygnus2/utility/generic_controller.dart';
 import 'package:flutter/material.dart';
@@ -38,12 +40,13 @@ class _MadCrudState extends State<MadCrud> {
 
   final scrollController = ScrollController();
 
-  final cNickname = GenericController<String>();
-  final cBirthday = GenericController<DateTime>();
-  final cBio = GenericController<String>();
+  final cNickname = TextEditingController();
 
-  final cUniversity = GenericController<String>();
-  final cDepartment = GenericController<String>();
+  final cBirthday = GenericController<DateTime>();
+  final cBio = TextEditingController();
+
+  final cUniversity = TextEditingController();
+  final cDepartment = TextEditingController();
 
   // bse64
   final cImg1 = GenericController<String>();
@@ -111,11 +114,11 @@ class _MadCrudState extends State<MadCrud> {
           idFirebase: widget.mad?.idFirebase,
           updated: DateTime.now(),
           personId: widget.myProfile!.profileData.idFirebase,
-          nickname: cNickname.value!,
+          nickname: cNickname.text,
           birthday: cBirthday.value!,
-          bio: cBio.value,
-          university: cUniversity.value!,
-          department: cDepartment.value!,
+          bio: cBio.text,
+          university: cUniversity.text,
+          department: cDepartment.text,
           img1: cImg1.value!,
           img2: cImg2.value,
           img3: cImg3.value,
@@ -143,6 +146,11 @@ class _MadCrudState extends State<MadCrud> {
 
   @override
   Widget build(BuildContext context) {
+    final languageCode = Localizations.localeOf(context).languageCode;
+
+    final years100 = DateTime.now().subtract(const Duration(days: 365 * 100));
+    final years18 = DateTime.now().subtract(const Duration(days: 365 * 18));
+
     return Scrollbar(
       thumbVisibility: true,
       controller: scrollController,
@@ -156,8 +164,61 @@ class _MadCrudState extends State<MadCrud> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                // Nickname
+                TextEditor(
+                  label: 'Nickname',
+                  controller: cNickname,
+                  required: true,
+                  readOnly: widget.readOnly,
+                ),
+                const SizedBox(height: 8),
+
+                // Birthday
+                DateEditor(
+                  label: 'Data di nascita',
+                  controller: cBirthday,
+                  minValue: years100,
+                  maxValue: years18,
+                  languageCode: languageCode,
+                  readOnly: widget.readOnly,
+                ),
+                const SizedBox(height: 8),
+
+                // University
+                TextEditor(
+                  label: 'Università',
+                  controller: cUniversity,
+                  required: true,
+                  minLength: 5,
+                  maxLength: 500,
+                  readOnly: widget.readOnly,
+                ),
+                const SizedBox(height: 8),
+
+                // Department
+                TextEditor(
+                  label: 'Dipartimento',
+                  controller: cDepartment,
+                  required: true,
+                  minLength: 3,
+                  maxLength: 500,
+                  readOnly: widget.readOnly,
+                ),
+                const SizedBox(height: 8),
+
+                // Bio
+                TextEditor(
+                  label: 'Biografia',
+                  controller: cBio,
+                  required: true,
+                  minLength: 3,
+                  maxLength: 500,
+                  readOnly: widget.readOnly,
+                ),
+                const SizedBox(height: 8),
+
                 // Province & City
-                const SizedBox(height: 16),
+                const SizedBox(height: 8),
                 const Text(
                   'Dove ti trovi? (è obbligatorio selezionare almeno uno tra comune o provincia)',
                   style: TextStyle(
