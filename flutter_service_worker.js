e394c8f28e8bc92744487a4901f0e63ca7f20a4b
mikymaione <mikymaione@users.noreply.github.com>
2024-03-27 08:50:55 +0000

    deploy: 961b777a36fbe1f37811eb3610b8eb29babfb696

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "419dfde6e593091cbd46fbf835684aa7",
-"/": "419dfde6e593091cbd46fbf835684aa7",
+const RESOURCES = {"index.html": "2a642f0679f017ef36c8b646515427ee",
+"/": "2a642f0679f017ef36c8b646515427ee",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
