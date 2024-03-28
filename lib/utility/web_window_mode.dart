1073a13bbd186a4da9ac4ee0de59f4da0a23c5b7
Michele Maione <mikymaione@hotmail.it>
2024-03-28 17:55:31 +0100

    refactor: navigateToUrl

--- lib/utility/web_window_mode.dart
+++ lib/utility/web_window_mode.dart
@@ -7,31 +7,14 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-import 'package:cygnus2/utility/web_window_mode.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+enum WebWindowMode {
+  self,
+  blank,
+}
 
-class Commons {
-  static printIfInDebug(Object? s) {
-    if (kDebugMode) {
-      print(s);
-    }
-  }
-
-  static Future<X?> navigate<X>({required BuildContext context, required WidgetBuilder builder}) => Navigator.push<X>(
-        context,
-        MaterialPageRoute(
-          builder: builder,
-        ),
-      );
-
-  static Future<void> navigateToUrl(String url, WebWindowMode webWindowMode) async {
-    final uri = Uri.parse(url);
-    final ok = await launchUrl(uri, webOnlyWindowName: webWindowMode.code);
-
-    if (!ok) {
-      throw Exception('Could not launch $url');
-    }
-  }
+extension WebWindowModeExtension on WebWindowMode {
+  String get code => switch (this) {
+        WebWindowMode.self => '_self',
+        WebWindowMode.blank => '_blank',
+      };
 }
